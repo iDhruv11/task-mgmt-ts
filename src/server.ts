b31ba64ca0eb1b37c625d5cfd37bed978dba994d
iDhruv11 <drvsrm11@gmail.com>
2025-05-16 15:54:45 +0530

    registrattion & login

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import { connectDB } from "./database";
 import client from "./database";
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Task Manager API Running");
@@ -25,6 +26,85 @@ app.get("/test-db", async (req, res) => {
     res.json({
       success: true,
       time: result.rows[0]
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      success: false
+    });
+  }
+});
+
+app.post("/register", async (req, res) => {
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    return res.status(400).json({
+      success: false,
+      message: "All fields required"
+    });
+  }
+
+  try {
+    await client.query(
+      `
+      INSERT INTO users(username,email,password)
+      VALUES($1,$2,$3)
+      `,
+      [username, email, password]
+    );
+
+    res.json({
+      success: true,
+      message: "User created"
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      success: false
+    });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({
+      success: false,
+      message: "All fields required"
+    });
+  }
+
+  try {
+    const result = await client.query(
+      `
+      SELECT *
+      FROM users
+      WHERE email = $1
+      `,
+      [email]
+    );
+
+    const user = result.rows[0];
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found"
+      });
+    }
+
+    if (user.password !== password) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid password"
+      });
+    }
+
+    res.json({
+      success: true,
+      user
     });
   } catch (error) {
     console.error(error);
